82e04432622866369b5a1993131ebc0282fdcba9
Min Win <minmmwin@gmail.com>
2021-06-10 21:58:31 -0400

    Update SQL_Joins.sql

--- SQL_Joins.sql
+++ SQL_Joins.sql
@@ -65,9 +65,9 @@ ON a.sales_rep_id = s.id
 WHERE s.name LIKE 'S%' AND r.name = 'Midwest'
 ORDER BY account;
 
-Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a last name starting with K and in the Midwest region. Your final
-table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according
-to account name.
+Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep 
+has a last name starting with K and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and 
+the account name. Sort the accounts alphabetically (A-Z) according to account name.
 SELECT r.name region , s.name rep, a.name account
 FROM sales_reps s
 JOIN region r
