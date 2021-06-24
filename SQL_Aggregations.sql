74e03cc1b6a9b88c6a64f0afc8e776337ce01b73
Min Win <minmmwin@gmail.com>
2021-06-24 18:50:28 -0400

    Update SQL_Aggregations.sql

--- SQL_Aggregations.sql
+++ SQL_Aggregations.sql
@@ -129,7 +129,8 @@ GROUP BY (r.name)
 ORDER BY number_of_sales_reps
 
 Questions: GROUP BY Part II
-Use the SQL environment below to assist with answering the following questions.
+Use the SQL environment below to assist with answering the following questions. Whether you get stuck or you just want to double check your
+solutions, my answers can be found at the top of the next concept.
 
 For each account, determine the average amount of each type of paper they purchased across their orders. Your result should have four columns
 - one for the account name and one for the average quantity purchased for each of the paper types for each account.
@@ -171,27 +172,97 @@ ON r.id = s.region_id
 GROUP BY r.name, w.channel
 ORDER BY num_events DESC;
 
-Questions: DISTINCT
+Questions: HAVING
 Use the SQL environment below to assist with answering the following questions.
 
-Use DISTINCT to test if there are any accounts associated with more than one region.
-SELECT a.name "account name", r.name "region name"
-FROM accounts a
-JOIN sales_reps s
-ON s.id = a.sales_rep_id
-JOIN region r
-ON r.id = s.region_id;
-
-SELECT DISTINCT id, name
-FROM accounts;
-
-Have any sales reps worked on more than one account?
-SELECT s.id "rep_id", s.name "rep", COUNT(*) num_accounts
+How many of the sales reps have more than 5 accounts that they manage?
+SELECT s.name, COUNT(a.id) number_of_accounts
 FROM sales_reps s
 JOIN accounts a
 ON a.sales_rep_id = s.id
-GROUP BY rep_id, rep
-ORDER BY num_accounts DESC;
+GROUP BY s.name
+HAVING COUNT(a.id) >5
+ORDER BY number_of_accounts
 
-SELECT DISTINCT id, name
-FROM sales_reps;
+How many accounts have more than 20 orders?
+SELECT a.id, a.name, COUNT(*) num_orders
+FROM accounts a
+JOIN orders o
+ON o.account_id = a.id
+GROUP BY a.id, a.name
+HAVING COUNT(*) >20
+ORDER BY num_orders;
+
+Which account has the most orders?
+SELECT a.id, a.name, COUNT(*) num_orders
+FROM accounts a
+JOIN orders o
+ON o.account_id = a.id
+GROUP BY a.id, a.name
+HAVING COUNT(*) >20
+ORDER BY num_orders DESC
+LIMIT 1;
+
+Which accounts spent more than 30,000 usd total across all orders?
+SELECT a.id, a.name, SUM(o.total_amt_usd) total_usd_spent
+FROM accounts a
+JOIN orders o
+ON o.account_id = a.id
+GROUP BY a.id, a.name
+HAVING SUM(o.total_amt_usd) > 30000
+ORDER BY total_usd_spent;
+
+Which accounts spent less than 1,000 usd total across all orders?
+SELECT a.id, a.name, SUM(o.total_amt_usd) total_usd_spent
+FROM accounts a
+JOIN orders o
+ON o.account_id = a.id
+GROUP BY a.id, a.name
+HAVING SUM(o.total_amt_usd) < 1000
+ORDER BY total_usd_spent;
+
+Which account has spent the most with us?
+SELECT a.id, a.name, SUM(o.total_amt_usd) total_spent
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+ORDER BY total_spent DESC
+LIMIT 1;
+
+Which account has spent the least with us?
+SELECT a.id, a.name, SUM(o.total_amt_usd) total_spent
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+ORDER BY total_spent
+LIMIT 1;
+
+Which accounts used facebook as a channel to contact customers more than 6 times?
+SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+GROUP BY a.id, a.name, w.channel
+HAVING COUNT(*) > 6 AND w.channel = 'facebook'
+ORDER BY use_of_channel;
+
+Which account used facebook most as a channel?
+SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+WHERE w.channel = 'facebook'
+GROUP BY a.id, a.name, w.channel
+ORDER BY use_of_channel DESC
+LIMIT 1;
+
+Which channel was most frequently used by most accounts?
+SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+GROUP BY a.id, a.name, w.channel
+ORDER BY use_of_channel DESC
+LIMIT 10;
